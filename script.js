325cb4317d154ba8fb94c4cef3b75478f516dac1
Zizu <ziblimzulka.zz@gmail.com>
2023-06-20 01:59:45 +0700

    Implement DOM manipulation

--- script.js
+++ script.js
@@ -1,145 +1,156 @@
+let rock = document.querySelector('#rock')
+let paper = document.querySelector('#paper')
+let scissors = document.querySelector('#scissors')
+let player_result = document.querySelector('.player-result')
+let computer_result = document.querySelector('.computer-result')
+let outcome = document.querySelector('.outcome')
+let round = document.querySelector('#round')
+let round_player = document.querySelector('#player-round')
+let round_computer = document.querySelector('#computer-round')
+
+
 let choices = ['Rock', 'Paper', 'Scissors'];
-let players = ['you', 'computer'];
-let decision = [];
-let message;
 let player_score = 0;
 let computer_score = 0;
+let computer_prefix = "Computer Score: ";
+let player_prefix = "Player Score: ";
+let round_num = 1;
+let player_round = 0
+let computer_round = 0
 
 // Function to determine computer's choice
 const getComputerChoice = () => {
     let choice = Math.floor(Math.random() * choices.length);
     return choices[choice].toLowerCase();
 }
+  
 
-// Function to detrmine the next player
-const who_to_play_next = () => {
-    let choice = Math.floor(Math.random() * players.length);
-    return players[choice];
-}
-
-// Function to determine winner or loser
-const playRound = (playerSelection, computerSelection) => {
-    // Randomly select the next player and append it's value first to the decision array before that of the next player
-    let player = who_to_play_next();
-    if(player == 'computer'){
-        computerSelection = getComputerChoice();
-        decision.push(computerSelection);
-        playerSelection = prompt('Enter your choice', 'Write your choice here.');
-        decision.push(playerSelection);
-
-        // Iterate the decision array and decide winner or loser.
-        if(decision[0].toLowerCase() === 'rock' && decision[1].toLowerCase() === 'scissors'){
-            message = `${player} wins. ${decision[0]} beats ${decision[1]}.`;
-            decision.pop();
-            decision.pop();
-            computer_score += 1;
-            return message;
-        }
-        else if(decision[0].toLowerCase() === 'scissors' && decision[1].toLowerCase() === 'paper'){
-            computer_score += 1;
-            message = `${player} wins. ${decision[0]} beats ${decision[1]}.`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-        else if(decision[0].toLowerCase() === 'paper' && decision[1].toLowerCase() === 'rock'){
-            computer_score += 1;
-            message = `${player} wins. ${decision[0]} beats ${decision[1]}.`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-        else if(decision[0].toLowerCase() === 'scissors' && decision[1].toLowerCase() === 'rock'){
-            player_score += 1;
-            message = `${player} loses. ${decision[1]} beats ${decision[0]}.`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-        else if(decision[0].toLowerCase() === 'paper' && decision[1].toLowerCase() === 'scissors'){
-            player_score += 1;
-            message = `${player} loses. ${decision[1]} beats ${decision[0]}.`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-        else if(decision[0].toLowerCase() === 'rock' && decision[1].toLowerCase() === 'paper'){
-            player_score += 1;
-            message = `${player} loses. ${decision[1]} beats ${decision[0]}.`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-        else{
-            message = `No winner. Play again`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-    }
-    else{
-        playerSelection = prompt('Enter your choice', 'Write your choice here.');
-        decision.push(playerSelection);
-        computerSelection = getComputerChoice();
-        decision.push(computerSelection);
-        
-        // Iterate the decision array and decide winner or loser.
-        if(decision[0].toLowerCase() === 'rock' && decision[1].toLowerCase() === 'scissors'){
-            player_score += 1;
-            message = `${player} win. ${decision[0]} beats ${decision[1]}.`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-        else if(decision[0].toLowerCase() === 'scissors' && decision[1].toLowerCase() === 'paper'){
-            player_score += 1;
-            message = `${player} win. ${decision[0]} beats ${decision[1]}.`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-        else if(decision[0].toLowerCase() === 'paper' && decision[1].toLowerCase() === 'rock'){
-            player_score += 1;
-            message = `${player} win. ${decision[0]} beats ${decision[1]}.`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-        else if(decision[0].toLowerCase() === 'scissors' && decision[1].toLowerCase() === 'rock'){
-            computer_score += 1;
-            message = `${player} lose. ${decision[1]} beats ${decision[0]}.`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-        else if(decision[0].toLowerCase() === 'paper' && decision[1].toLowerCase() === 'scissors'){
-            computer_score += 1;
-            message = `${player} lose. ${decision[1]} beats ${decision[0]}.`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-        else if(decision[0].toLowerCase() === 'rock' && decision[1].toLowerCase() === 'paper'){
-            computer_score += 1;
-            message = `${player} lose. ${decision[1]} beats ${decision[0]}.`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-        else{
-            message = `No winner. Play again`;
-            decision.pop();
-            decision.pop();
-            return message;
-        }
-    }
-}
-
-const game = () => {
+const rockPlayRound = () => {
     let computerSelection = getComputerChoice();
-    let playerSelection = prompt('Enter your choice', 'Write your choice here.');
-    while(player_score < 5 || computer_score < 5){
-        console.log(playRound(playerSelection, computerSelection));
+    let playerSelection = "rock";
+
+    if(player_score < 5 && computer_score < 5){
+        if(playerSelection === computerSelection){
+            player_score += 0;
+            computer_score += 0;
+            player_result.textContent = player_prefix + player_score;
+            computer_result.textContent = computer_prefix + computer_score;
+            outcome.textContent = "It is a draw";
+        }
+    
+        if(computerSelection.toLowerCase() === "scissors"){
+            player_score += 1;
+            player_result.textContent = player_prefix + player_score;
+            computer_result.textContent = computer_prefix + computer_score;
+            outcome.textContent = `${playerSelection} beats ${computerSelection} this round`;
+        }
+    
+        if(computerSelection.toLowerCase() === "paper"){
+            computer_score += 1;
+            player_result.textContent = player_prefix + player_score;
+            computer_result.textContent = computer_prefix + computer_score;
+            outcome.textContent = `${playerSelection} beats ${computerSelection} this round`;
+        }
     }
-    return player_score > computer_score ? 'You win the game' : 'Computer wins the game'
+    else {
+        outcome.textContent = player_score === 5 && computer_score < 5 ? "You win. Game over!!!" : "Computer wins. Game over!!!";
+        player_score = 0;
+        computer_score = 0;
+    }
 }
+
+const paperPlayRound = () => {
+    let computerSelection = getComputerChoice();
+    let playerSelection = "paper";
+
+    if(player_score < 5 && computer_score < 5){
+        if(playerSelection === computerSelection){
+            player_score += 0;
+            computer_score += 0;
+            player_result.textContent = player_prefix + player_score;
+            computer_result.textContent = computer_prefix + computer_score;
+            outcome.textContent = "It is a draw"; 
+        }
+    
+        if(computerSelection.toLowerCase() === "rock"){
+            player_score += 1;
+            player_result.textContent = player_prefix + player_score;
+            computer_result.textContent = computer_prefix + computer_score;
+            outcome.textContent = `${playerSelection} beats ${computerSelection} this round`;
+        }
+    
+        if(computerSelection.toLowerCase() === "scissors"){
+            computer_score += 1;
+            player_result.textContent = player_prefix + player_score;
+            computer_result.textContent = computer_prefix + computer_score;
+            outcome.textContent = `${playerSelection} beats ${computerSelection} this round`;
+        }
+    }
+    else {
+        outcome.textContent = player_score === 5 && computer_score < 5 ? "You win. Game over!!!" : "Computer wins. Game over!!!";
+        player_score = 0;
+        computer_score = 0;
+    }
+}
+
+const scissorsPlayRound = () => {
+    let computerSelection = getComputerChoice();
+    let playerSelection = "scissors";
+
+    if(player_score < 5 && computer_score < 5){
+        if(playerSelection === computerSelection){
+            player_score += 0;
+            computer_score += 0;
+            player_result.textContent = player_prefix + player_score;
+            computer_result.textContent = computer_prefix + computer_score;
+            outcome.textContent = "It is a draw";
+        }
+    
+        if(computerSelection.toLowerCase() === "paper"){
+            player_score += 1;
+            player_result.textContent = player_prefix + player_score;
+            computer_result.textContent = computer_prefix + computer_score;
+            outcome.textContent = `${playerSelection} beats ${computerSelection} this round`;
+        }
+    
+        if(computerSelection.toLowerCase() === "rock"){
+            computer_score += 1;
+            player_result.textContent = player_prefix + player_score;
+            computer_result.textContent = computer_prefix + computer_score;
+            outcome.textContent = `${computerSelection} beats ${playerSelection} this round`;
+        }
+    }
+    else {
+        outcome.textContent = player_score === 5 && computer_score < 5 ? "You win. Game over!!!" : "Computer wins. Game over!!!";
+        player_score = 0;
+        computer_score = 0;
+    }
+}
+
+// if(round_num >= 3){
+//     round.textContent = `Round ${round_num}`
+//     outcome.textContent = player_round === 5 && computer_round < 5 ? "You win. Game over!!!" : "Computer wins. Game over!!!";
+//     player_score = 0;
+//     computer_score = 0;
+//     player_result.textContent = '';
+//     computer_result.textContent = '';
+//     round_player.textContent = '';
+//     round_computer.textContent = '';
+// }
+// else{
+//     round.textContent = `Round ${round_num}`
+//     player_round = player_score >= 5 ? player_round + 1 : player_round;
+//     computer_round = computer_score >= 5 ? computer_round + 1 : computer_round;
+//     round_num = player_round + computer_round
+//     round_player.textContent =`Player Score: ${player_round}`
+//     round_computer.textContent = `Computer Score: ${computer_round}`
+// }
+
+rock.addEventListener('click', rockPlayRound);
+paper.addEventListener('click', paperPlayRound);
+scissors.addEventListener('click', scissorsPlayRound);
+
+
+
+
+
